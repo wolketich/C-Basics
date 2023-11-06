b7c186f692c1a94f6171dc5207e55473446cb403
Vladislav Cernega <vladislav@walkcat.net>
2023-11-06 22:23:57 +0000

    Lab Usage Code improvement

--- Chapter7/labUsage_2dArray.c
+++ Chapter7/labUsage_2dArray.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 
+#define DAYS_IN_WEEK 7
+#define LABS 5
+
 int main(int argc, char *argv[]){
-  int totalUsage;
-  int usage[7][5];
+  int totalUsage = 0;
+  int usage[DAYS_IN_WEEK][LABS] = {0}; // Initialize all elements to 0
 
   // Read lab's usage for each day
-  for (int day = 0; day < 7; day++) {
-    printf("\nEntering the usage of day #%d.\n", day+1);
+  for (int day = 0; day < DAYS_IN_WEEK; day++) {
+    printf("\nEntering the usage of day #%d.\n", day + 1);
     printf("-----------------------------\n");
-    for (int lab = 0; lab < 5; lab++) {
-      printf("Enter usage of lab #%d for day %d: ", lab+1, day+1);
-      scanf("%d", &usage[day][lab]);
+    for (int lab = 0; lab < LABS; lab++) {
+      printf("Enter usage of lab #%d for day %d: ", lab + 1, day + 1);
+      if (scanf("%d", &usage[day][lab]) != 1) {
+        // Handle input error
+        fprintf(stderr, "Error reading input for lab %d, day %d\n", lab + 1, day + 1);
+        return 1;
+      }
     }
   }
 
   // Calculate the average usage of every lab
   putchar('\n');
-  putchar('\n');
   printf("Calculating the average laboratory usage.\n");
-  printf("--------------------------------\n");
-  for (int lab = 0; lab < 5; lab++){
-    totalUsage = 0;
-    for (int day = 0; day < 7; day++){
+  printf("----------------------------------------\n");
+  for (int lab = 0; lab < LABS; lab++){
+    totalUsage = 0; // Reset to 0 for each lab
+    for (int day = 0; day < DAYS_IN_WEEK; day++){
       totalUsage += usage[day][lab];
     }
-    printf("Average usage for Lab #%d is %.2f.\n", lab + 1, totalUsage / 7.0);
+    printf("Average usage for Lab #%d is %.2f.\n", lab + 1, (float)totalUsage / DAYS_IN_WEEK);
   }
 
   return 0;
